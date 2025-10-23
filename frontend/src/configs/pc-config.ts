f08801f38575c232bef77684bf6e493fbe6980e0
Jan Altrichter <xaltrich@fi.muni.cz>
2025-10-23 18:17:32 +0200

    Leave a single turn server

--- frontend/src/configs/pc-config.ts
+++ frontend/src/configs/pc-config.ts
@@ -1,23 +1,23 @@
 export const PCConfig = {
     iceServers: [
-        {
-            urls: "stun:stun.relay.metered.ca:80",
-        },
-        {
-            urls: "turn:standard.relay.metered.ca:80",
-            username: "bc9997c2871a647d2bf4ffc1",
-            credential: "zfqpGjMnRwBcwNS1",
-        },
-        {
-            urls: "turn:standard.relay.metered.ca:80?transport=tcp",
-            username: "bc9997c2871a647d2bf4ffc1",
-            credential: "zfqpGjMnRwBcwNS1",
-        },
-        {
-            urls: "turn:standard.relay.metered.ca:443",
-            username: "bc9997c2871a647d2bf4ffc1",
-            credential: "zfqpGjMnRwBcwNS1",
-        },
+        // {
+        //     urls: "stun:stun.relay.metered.ca:80",
+        // },
+        // {
+        //     urls: "turn:standard.relay.metered.ca:80",
+        //     username: "bc9997c2871a647d2bf4ffc1",
+        //     credential: "zfqpGjMnRwBcwNS1",
+        // },
+        // {
+        //     urls: "turn:standard.relay.metered.ca:80?transport=tcp",
+        //     username: "bc9997c2871a647d2bf4ffc1",
+        //     credential: "zfqpGjMnRwBcwNS1",
+        // },
+        // {
+        //     urls: "turn:standard.relay.metered.ca:443",
+        //     username: "bc9997c2871a647d2bf4ffc1",
+        //     credential: "zfqpGjMnRwBcwNS1",
+        // },
         {
             urls: "turns:standard.relay.metered.ca:443?transport=tcp",
             username: "bc9997c2871a647d2bf4ffc1",
