6756af4613c97600866828835a04913a4c57461a
Jan Altrichter <xaltrich@fi.muni.cz>
2025-10-25 13:41:26 +0200

    Remove disconnected users

--- frontend/src/configs/pc-config.ts
+++ frontend/src/configs/pc-config.ts
@@ -1,23 +1,23 @@
 export const PCConfig = {
     iceServers: [
-        // {
-        //     urls: "stun:stun.relay.metered.ca:80",
-        // },
-        // {
-        //     urls: "turn:standard.relay.metered.ca:80",
-        //     username: "894edd5b6adf9a3102935f64",
-        //     credential: "6ZSXB3HxCMFXRfFX",
-        // },
+        {
+            urls: "stun:stun.relay.metered.ca:80",
+        },
+        {
+            urls: "turn:standard.relay.metered.ca:80",
+            username: "894edd5b6adf9a3102935f64",
+            credential: "6ZSXB3HxCMFXRfFX",
+        },
         {
             urls: "turn:standard.relay.metered.ca:80?transport=tcp",
             username: "894edd5b6adf9a3102935f64",
             credential: "6ZSXB3HxCMFXRfFX",
         },
-        // {
-        //     urls: "turn:standard.relay.metered.ca:443",
-        //     username: "894edd5b6adf9a3102935f64",
-        //     credential: "6ZSXB3HxCMFXRfFX",
-        // },
+        {
+            urls: "turn:standard.relay.metered.ca:443",
+            username: "894edd5b6adf9a3102935f64",
+            credential: "6ZSXB3HxCMFXRfFX",
+        },
         {
             urls: "turns:standard.relay.metered.ca:443?transport=tcp",
             username: "894edd5b6adf9a3102935f64",
